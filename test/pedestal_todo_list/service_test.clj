1305fe542d3e0ec1728db3a8e2360a3e8a84fd51
FeliPython <felipe.morais@nubank.com.br>
2020-07-19 15:33:18 -0300

    migrate tests to Midje

--- test/pedestal_todo_list/service_test.clj
+++ test/pedestal_todo_list/service_test.clj
@@ -1,47 +1,40 @@
 (ns pedestal-todo-list.service-test
-  (:use midje.sweet)
   (:require [clojure.test :refer :all]
-            [io.pedestal.test :refer :all]
+            [io.pedestal.test :refer [response-for]]
             [io.pedestal.http :as bootstrap]
-            [pedestal-todo-list.service :as service]))
-
-
-(fact
- (1 => 2))
-
+            [pedestal-todo-list.service :as service]
+            [midje.sweet :refer [fact facts]]
+            [midje.checkers :refer [contains]]))
 
 (def service
   (::bootstrap/service-fn (bootstrap/create-servlet service/service)))
 
-(deftest home-page-test
-  (is (=
-       (:body (response-for service :get "/"))
-       "Hello Felip!"))
-  (is (/ 1 0))
-  (is (=
-       (:headers (response-for service :get "/"))
-       {"Content-Type"                      "text/html;charset=UTF-8"
+(facts "home page test"
+  (fact
+    (:body (response-for service :get "/")) => "Hello Felipe!")
+
+  (fact
+    (:headers (response-for service :get "/")) =>
+    {"Content-Type"                      "text/html;charset=UTF-8"
+     "Strict-Transport-Security"         "max-age=31536000; includeSubdomains"
+     "X-Frame-Options"                   "DENY"
+     "X-Content-Type-Options"            "nosniff"
+     "X-XSS-Protection"                  "1; mode=block"
+     "X-Download-Options"                "noopen"
+     "X-Permitted-Cross-Domain-Policies" "none"
+     "Content-Security-Policy"           "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"}))
+
+(facts "about-page-test"
+  (fact "check clojure version"
+    (:body (response-for service :get "/about")) => (contains "Clojure 1.10.1"))
+
+  (fact "check headers"
+    (:headers (response-for service :get "/about"))
+    => {"Content-Type"                      "text/html;charset=UTF-8"
         "Strict-Transport-Security"         "max-age=31536000; includeSubdomains"
         "X-Frame-Options"                   "DENY"
         "X-Content-Type-Options"            "nosniff"
         "X-XSS-Protection"                  "1; mode=block"
         "X-Download-Options"                "noopen"
         "X-Permitted-Cross-Domain-Policies" "none"
-        "Content-Security-Policy"           "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"})))
-
-
-(deftest about-page-test
-  (is (.contains
-       (:body (response-for service :get "/about"))
-       "Clojure 1.10.1"))
-  (is (=
-       (:headers (response-for service :get "/about"))
-       {"Content-Type"                      "text/html;charset=UTF-8"
-        "Strict-Transport-Security"         "max-age=31536000; includeSubdomains"
-        "X-Frame-Options"                   "DENY"
-        "X-Content-Type-Options"            "nosniff"
-        "X-XSS-Protection"                  "1; mode=block"
-        "X-Download-Options"                "noopen"
-        "X-Permitted-Cross-Domain-Policies" "none"
-        "Content-Security-Policy"           "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"})))
-
+        "Content-Security-Policy"           "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;"}))
