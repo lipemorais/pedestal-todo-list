390dfdf64ed0e21ffb3b90d00e5736bb708cf651
FeliPython <felipe.morais@nubank.com.br>
2020-07-19 15:58:53 -0300

    remove printing routes

--- src/pedestal_todo_list/server.clj
+++ src/pedestal_todo_list/server.clj
@@ -2,7 +2,6 @@
   (:gen-class)                                              ; for -main method in uberjar
   (:require [io.pedestal.http :as server]
             [io.pedestal.http.route :as route]
-            [io.pedestal.http.route.definition.table :refer [table-routes]]
             [pedestal-todo-list.service :as service]))
 
 ;; This is an adapted service map, that can be started and stopped
@@ -29,55 +28,6 @@
       server/dev-interceptors
       server/create-server
       server/start))
-;
-;
-;(defn print-routes
-;  "Print our application's routes"
-;  []
-;  (route/print-routes (table-routes service/routes)))
-
-(defn recognize-route
-  "Verifies the requested HTTP verb and path are recognized by the router."
-  [verb path]
-  (route/try-routing-for (table-routes service/routes) :prefix-tree path verb))
-
-(defn named-route
-  "Finds a route by name"
-  [route-name]
-  (->> service/routes
-       table-routes
-       (filter #(= route-name (:route-name %)))
-       first))
-
-(defn print-route
-  "Prints a route and its interceptors"
-  [rname]
-  (letfn [(joined-by
-            [s coll]
-            (apply str (interpose s coll)))
-
-          (repeat-str
-            [s n]
-            (apply str (repeat n s)))
-
-          (interceptor-info
-            [i]
-            (let [iname  (or (:name i) "<handler>")
-                  stages (joined-by
-                          ","
-                          (keys
-                           (filter
-                            (comp (complement nil?) val)
-                            (dissoc i :name))))]
-              (str iname " (" stages ")")))]
-    (when-let [rte (named-route rname)]
-      (let [{:keys [path method route-name interceptors]} rte
-            name-line (str "[" method " " path " " route-name "]")]
-        (joined-by
-         "\n"
-         (into [name-line (repeat-str "-" (count name-line))]
-               (map interceptor-info interceptors)))))))
-
 
 (defn -main
   "The entry-point for 'lein run'"
